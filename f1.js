12d1f484e1c5479ab94eae82d0c26b01892a8310
Linus <linus.blomgren@skola.taby.se>
2023-05-13 18:27:25 +0200

    typklar

--- f1.js
+++ f1.js
@@ -1,8 +1,13 @@
 let knapp = document.getElementById("knapp");
 let rabatt = document.getElementById("Rabatt");
 
-
-
 knapp.addEventListener("click", ()=>{
   rabatt.insertAdjacentHTML("beforeend", `<div>Du har rabatt på 20% och din rabattkod är: Heja Linus!</div>`)
 })
+
+// _______________________________
+
+L.mapbox.accessToken = 'pk.eyJ1IjoiYmxvbW1hbjI0IiwiYSI6ImNsaG01em42ajB1N3czZW9wZG9nYTNseGQifQ.uU567uzw4uTN_33G7-Md1g';
+var map = L.mapbox.map('map')
+    .setView([59.4, 17.9], 9)
+    .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
