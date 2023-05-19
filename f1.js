b6542715224a1fca3c810f3910bd938728c5ce0b
Linus <linus.blomgren@skola.taby.se>
2023-05-19 19:55:41 +0200

    klar

--- f1.js
+++ f1.js
@@ -2,7 +2,7 @@ let knapp = document.getElementById("knapp");
 let rabatt = document.getElementById("Rabatt");
 
 knapp.addEventListener("click", ()=>{
-  rabatt.insertAdjacentHTML("beforeend", `<div>Du har rabatt på 20% och din rabattkod är: Heja Linus!</div>`)
+  rabatt.insertAdjacentHTML("beforeend", `<div>Rabattkod: HejaLinus! , på alla butiker i Stockholm</div>`)
 })
 
 // _______________________________
